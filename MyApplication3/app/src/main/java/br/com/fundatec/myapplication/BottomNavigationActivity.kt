6879d17a25a2a6db9c812057c0bba346e8964d10
Matheus <matheusbj95@gmail.com>
2023-01-20 21:43:10 -0300

    criado a classe BottomNavigationActivity.kt e adicionada à activity no AndroidManifest.xml, criado activity_bottom_navigation.xml e feito os botões de navegação junto da bottom_nav_menu.xml, adicionado as svg round_bell_svgrepo_com.xml e white_home_svgrepo_com.xml e adicionado a cor vermelho na activity_home.xml

---
+++ MyApplication3/app/src/main/java/br/com/fundatec/myapplication/BottomNavigationActivity.kt
@@ -0,0 +1,42 @@
+package br.com.fundatec.myapplication
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import br.com.fundatec.myapplication.databinding.ActivityBottomNavigationBinding
+
+class BottomNavigationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBottomNavigationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       /* binding.navigation.setOnItemSelectedListener {
+            onNavigationSelected(it)
+            true
+        }*/
+
+        binding.navigation.selectedItemId = R.id.navigation_home
+
+    }
+
+   /* private fun onNavigationSelected(item: MenuItem) {
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                openFragment(HomeFragment.newInstance())
+            }
+            else -> {
+                openFragment(NotificationFragment.newInstance())
+            }
+        }
+    }*/
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
