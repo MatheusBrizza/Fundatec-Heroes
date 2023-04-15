f258e0e94631ad6d12cc2fc3e6e8d8256e08de1c
Matheus <matheusbj95@gmail.com>
2023-04-15 02:45:31 -0300

    Login + padronização de cor do tema + validações dos campos da tela de login

--- MyApplication3/app/src/main/java/br/com/fundatec/myapplication/login/presentation/LoginViewModel.kt
+++ MyApplication3/app/src/main/java/br/com/fundatec/myapplication/login/presentation/LoginViewModel.kt
@@ -3,24 +3,29 @@ package br.com.fundatec.myapplication.login.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.fundatec.components.EmailValidator
 
-class LoginViewModel :ViewModel() {
+class LoginViewModel : ViewModel() {
     private val state = MutableLiveData<ViewState>()
     val viewState: LiveData<ViewState> = state
 
     fun validateUserInput(email: String?, password: String?) {
         if (email.isNullOrEmpty() && password.isNullOrEmpty()) {
-            state.value = ViewState.ShowError
-        }
-        else {
-         state.value = ViewState.ShowHome
+            state.value = ViewState.ShowErrorFields
+        } else if (email?.let { EmailValidator.isEmailValid(email) } == false) {
+            state.value = ViewState.ShowErrorEmail
+        } else if (password.isNullOrEmpty() || password.length < 6) {
+            state.value = ViewState.ShowErrorPassword
+        } else {
+            state.value = ViewState.ShowHome
         }
     }
+
 }
 
 sealed class ViewState {
     object ShowHome : ViewState()
-    object ShowError : ViewState()
+    object ShowErrorFields : ViewState()
     object ShowErrorEmail : ViewState()
     object ShowErrorPassword : ViewState()
 }
--- MyApplication3/app/src/main/java/br/com/fundatec/myapplication/login/view/LoginActivity.kt
+++ MyApplication3/app/src/main/java/br/com/fundatec/myapplication/login/view/LoginActivity.kt
@@ -4,31 +4,18 @@ package br.com.fundatec.myapplication.login.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.constraintlayout.widget.ConstraintLayout
+import br.com.fundatec.components.showSnack
+import br.com.fundatec.components.showToast
 import br.com.fundatec.myapplication.home.view.HomeActivity
-import br.com.fundatec.myapplication.R
 import br.com.fundatec.myapplication.databinding.ActivityLoginBinding
 import br.com.fundatec.myapplication.login.presentation.LoginViewModel
 import br.com.fundatec.myapplication.login.presentation.ViewState
-import com.google.android.material.snackbar.Snackbar
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import br.com.fundatec.myapplication.character.view.Character
+import br.com.fundatec.myapplication.profile.view.ProfileActivity
 
 class LoginActivity : AppCompatActivity() {
 
-    private val moshi by lazy {
-        Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-    }
 
-    /*private val character by lazy {
-        Character("Batata", "asd")
-    }
-*/
     private lateinit var binding: ActivityLoginBinding
 
     private val viewModel: LoginViewModel by viewModels()
@@ -39,56 +26,42 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-/*
-        val preferences = getSharedPreferences("bd", MODE_PRIVATE)
-        val characterString = moshi.adapter(Character::class.java).toJson(character)
-        preferences.edit().putString("character", characterString).commit()
-*/
         binding.etEmail.error = "Campo obrigatório"
+        binding.etPassword.error = "Campo obrigatório"
 
-        configLoginButton()
+
         viewModel.viewState.observe(this) { state ->
             when (state) {
                 is ViewState.ShowHome -> showHome()
-                is ViewState.ShowError -> showSnack()
-                is ViewState.ShowErrorEmail -> {}
-                is ViewState.ShowErrorPassword -> {}
+                is ViewState.ShowErrorFields -> showSnack(binding.root, "campos não devem estar vazios")
+                is ViewState.ShowErrorEmail -> showToast("email incorreto")
+                is ViewState.ShowErrorPassword -> showToast("senha incorreta")
             }
+        }
+        configLoginButton()
+        configProfileButton()
+    }
+
+    private fun configLoginButton() {
+        binding.btnLogin.setOnClickListener {
+            viewModel.validateUserInput(
+                email = binding.etEmail.text.toString(),
+                password = binding.etPassword.text.toString(),
+            )
         }
     }
 
-        private fun configLoginButton() {
-            binding.btnLogin.setOnClickListener {
-                viewModel.validateUserInput(
-                    email = binding.etEmail.text.toString(),
-                    password = binding.etPassword.text.toString(),
-                )
-            }
+    private fun configProfileButton() {
+        binding.tvNovoUsuario.setOnClickListener {
+            showProfile()
         }
-
-
-//        findViewById<TextView>(R.id.tv_text).setOnClickListener {
-//            showToast()
-//            showSnack()
-//        }
-//    }
-
-    private fun showToast() {
-        Toast.makeText(this, "Olá!", Toast.LENGTH_SHORT)
-    }
-
-    private fun showSnack() {
-        val container = findViewById<ConstraintLayout>(R.id.container)
-        Snackbar
-            .make(container, "Nosso snackbar", Snackbar.LENGTH_LONG)
-            .setAction("OK") {
-                showHome()
-            }
-            .show()
     }
 
     private fun showHome() {
-        val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-        startActivity(intent)
+        startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+    }
+
+    private fun showProfile() {
+        startActivity(Intent(this@LoginActivity, ProfileActivity::class.java))
     }
 }
